8e5f881065681cfdbd1663402b8d624e915847fb
rmendev <rmendev_13@outlook.com>
2022-06-05 23:57:33 -0300

    Adicionado mais um teste para a classe Endereço

--- test/EnderecoTest.java
+++ test/EnderecoTest.java
@@ -1,6 +1,7 @@
 package test.java.org.fpij.jitakyoei.model.beans;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.fpij.jitakyoei.model.beans.Endereco;
 import org.junit.Test;
@@ -15,5 +16,14 @@ public class EnderecoTest
         endereco_teste.setCep("11345350");
 
         assertEquals(endereco_teste.getCep().length(), 8);
+    }
+
+    @Test
+    public void verificarNumeroCEP() {
+
+        Endereco endereco_teste = new Endereco();
+        endereco_teste.setCep("1134535j");
+
+        assertTrue(endereco_teste.getCep().matches("[a-zA-Z]+"));
     }
 }
