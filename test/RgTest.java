938ad7fd7c398c044c1d13d111fdf7df15bb6507
rmendev <rmendev_13@outlook.com>
2022-06-05 23:13:04 -0300

    Atualização do teste de RG

--- test/RgTest.java
+++ test/RgTest.java
@@ -1,6 +1,6 @@
 package test.java.org.fpij.jitakyoei.model.beans;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.fpij.jitakyoei.model.beans.Rg;
 import org.junit.Test;
@@ -11,12 +11,10 @@ public class RgTest
     @Test
     public void verificarRgVazio() {
 
-        // setup inicial
         Rg rg_teste = new Rg();
         
         rg_teste.setNumero("106601507");
 
-        // (29 * 7) + 1234567 deve resultar em 1234770
         assertNotEquals(rg_teste.getNumero(), "");
     }
 }
